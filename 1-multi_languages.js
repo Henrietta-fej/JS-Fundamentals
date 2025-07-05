10221b7999e9f94ab949224b36fd6df0dcbb8056
Oghenefejiro H. Aligbo <herrietaligbo@gmail.com>
2025-07-05 04:17:05 -0700

    Add print 3 programming languages

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
-const line1 = "c is fun";
-const line2 ="python is cool";
-const line3 = "javascript is amazing";
-console.log (line1);
-console.log (line2);
-console.log (line3);
+const line1 ="C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
