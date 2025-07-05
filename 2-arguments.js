3f228e632c0dcd6a95c20ebe9758ff6533c46a23
Oghenefejiro H. Aligbo <herrietaligbo@gmail.com>
2025-07-05 06:13:44 -0700

    Add  2-arguments.js script

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.log ("no argument");
+    if(args.length === 1 );
+    console.log ("argument found");
+}else{
+    console.log ("argument found");
+}
+    
